d85b9d4fa5a777ab02117249fe36159c6ddc9864
Vincent Melara <viincentmelara@gmail.com>
2024-05-14 01:22:16 -0700

    fixed server issue

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function init() {
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
+    cube.rotation.x += 1;
     cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
